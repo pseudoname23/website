ae97eb7c48a1f1b4dc517fc9374eb481502f7fa1
pseudoname <ryan.c.deken@gmail.com>
2025-01-21 11:36:15 -0600

    easter egg

--- trumpclock/clock.js
+++ trumpclock/clock.js
@@ -2,5 +2,8 @@ const $ = id => document.getElementById(id);
 
 const title = $("title");
 title.addEventListener("pointerup", () => {
-    console.log(this);
-})
+    title.innerText = "Shadow President Musk's term ends in"
+}, {once: true});
+
+const inaugurationDate = new Date(2025, 0, 20, 11);
+const endDate = new Date(2029, 0, 20, 11);
