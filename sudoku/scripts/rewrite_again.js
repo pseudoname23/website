bb65de86300a5761117f5b3b9dc285621ac0517e
pseudoname <ryan.c.deken@gmail.com>
2025-03-06 12:19:01 -0600

    it just decided to work now

--- sudoku/scripts/rewrite_again.js
+++ sudoku/scripts/rewrite_again.js
@@ -6,11 +6,7 @@ function selectCell(cellDOM, add) {
   if (!add) {
     const arrayCopy = Array.from(document.getElementsByClassName("selected"));
     for (const selectedCell of arrayCopy) {
-      if (selectedCell !== cellDOM) {
-        selectedCell.classList.remove("selected")
-      } else {
-        console.log("Skipping matching element")
-      };
+      if (selectedCell !== cellDOM) selectedCell.classList.remove("selected")
     }
   }
   cellDOM.classList.toggle("selected");
