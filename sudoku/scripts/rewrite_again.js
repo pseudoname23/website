47676d7c36ac3a88162ea5adfd4f5a3a5fbac41b
pseudoname <ryan.c.deken@gmail.com>
2025-03-27 12:25:13 -0500

    straight up solving it

--- sudoku/scripts/rewrite_again.js
+++ sudoku/scripts/rewrite_again.js
@@ -236,6 +236,42 @@ function setRandomCell() {
 
 $("setnum").addEventListener("pointerup", setRandomCell);
 
+// AUTO SOLVING 
+function attemptSolve() {
+  let puzzleChanged = false;
+  const emptyCellMap = {};
+  Array.from(gridInternal.emptyCells).forEach(cell => {
+    emptyCellMap[hash(cell.x, cell.y)] = cell.getPossibleNumbers();
+  })
+  for (const hash in emptyCellMap) {
+    const possibleNumbers = emptyCellMap[hash];
+    const cell = gridInternal.cells[hash];
+    if (possibleNumbers.length > 1) continue;
+    if (possibleNumbers.length === 1) {
+      cell.setTo(possibleNumbers[0]);
+      if (cell.number === null) {
+        // If cell is still null, that means its only possible number is no longer possible, leaving the puzzle unsolvable
+        console.warn(`Sudoku is unsolvable: Cell ${hash} has no possible numbers. `, cell)
+        cell.DOM.classList.add("unsolvable");
+      } else {
+        puzzleChanged = true;
+      }
+    }
+    if (possibleNumbers.length === 0) {
+      // If the cell has no possible numbers, the puzzle is unsolvable
+      console.warn(`Sudoku is unsolvable: Cell ${hash} has no possible numbers. `, cell)
+      cell.DOM.classList.add("unsolvable");
+    }
+  }
+  console.log(`Puzzle changed: ${puzzleChanged.toString()}`);
+  return puzzleChanged;
+  /*{
+      "ab": [1, 2, 3]
+      "cd": [4, 5]
+      "ef": [6]
+  */
+}
+
 window.addEventListener("keyup", e => {
   setCells(e.code);
   clearCells(e.code);
